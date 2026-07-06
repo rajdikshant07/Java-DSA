c90c60a5e750773197a84e486791c490beca9f93
Dikshant Raj <143262228+rajdikshant07@users.noreply.github.com>
2026-07-06 11:10:18 +0530

    Add files via upload

---
+++ Leetcode217.java
@@ -0,0 +1,13 @@
+//leetcode 217 - contains duplicate
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
---
+++ Leetcode771.java
@@ -0,0 +1,19 @@
+//leetcode 771 - Jewels and Stones
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewelSet = new HashSet<>();
+        for (char jewel : jewels.toCharArray()) {
+            jewelSet.add(jewel);
+        }
+        
+        int count = 0;
+        for (char stone : stones.toCharArray()) {
+            if (jewelSet.contains(stone)) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
